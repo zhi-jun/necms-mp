7fad2670da90875e4b1a1f434dd0c1c32eb7b2e2
张志军 <zjzhang2.iflytek.com>
2021-03-20 22:10:07 +0800

    日历控件

--- pages/car-list/car-list.js
+++ pages/car-list/car-list.js
@@ -1,26 +1,37 @@
 Page({
   data: {
-    inputShowed: false,
-    inputVal: "",
-    carList: [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    value: '',
+    result: [
+      { no: 'xxxxxsdsdsd', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' },
+      { no: 'xxxxxsdsds111d', org: '企业+下级', group: '所属分组：', cardType: '', cardNo: '车牌号', totalDriverTime: '总行驶时长', intervalDriverTime: '区间行驶时', todayDriverTime: '今日行驶时长' }
+    ]
   },
-  onLoad() {
+
+  onConfirm() {
+    this.selectComponent('#item').toggle();
+  },
+
+  onSwitch1Change({ detail }) {
+    this.setData({ switch1: detail });
+  },
+
+  onSwitch2Change({ detail }) {
+    this.setData({ switch2: detail });
+  },
+
+  onChange(e) {
     this.setData({
-      search: this.search.bind(this)
-    })
+      value: e.detail,
+    });
   },
-  search: function (value) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.searchData()
-        resolve()
-      }, 200)
-    })
-  },
-  selectResult: function (e) {
-    wx.showToast({ title: this.detail })
-  },
-  searchData() {
-    this.carList = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+  onSearch() {
+    if (!this.data.value)
+      return
+    console.log('搜索' + this.data.value);
   }
-});
+})
