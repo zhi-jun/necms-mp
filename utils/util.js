8996f450da6e8759b1698eafca0a205f9e8d2200
flydancing <277584734@qq.com>
2021-03-21 17:30:21 +0800

    feat: 地图控件轨迹播放

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,53 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+function convert2TecentMap(lng, lat) {
+  if (lng == '' && lat == '') {
+    return {
+      lng: '',
+      lat: ''
+    }
+  }
+  var x_pi = 3.14159265358979324 * 3000.0 / 180.0
+  var x = lng - 0.0065
+  var y = lat - 0.006
+  var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * x_pi)
+  var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * x_pi)
+  var qqlng = z * Math.cos(theta)
+  var qqlat = z * Math.sin(theta)
+  return {
+    lng: qqlng,
+    lat: qqlat
+  }
+}
+
+const compareVersion = (v1, v2) => {
+	v1 = v1.split('.');
+	v2 = v2.split('.');
+	const len = Math.max(v1.length, v2.length);
+
+	while (v1.length < len) {
+		v1.push('0');
+	}
+	while (v2.length < len) {
+		v2.push('0');
+	}
+
+	for (let i = 0; i < len; i++) {
+		const num1 = parseInt(v1[i]);
+		const num2 = parseInt(v2[i]);
+
+		if (num1 > num2) {
+			return 1;
+		} else if (num1 < num2) {
+			return -1;
+		}
+	}
+
+	return 0;
+};
 module.exports = {
-  formatTime
+  formatTime,
+  convert2TecentMap,
+  compareVersion
 }
