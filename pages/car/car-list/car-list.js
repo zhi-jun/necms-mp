0c30797ae9684aa5109ef1428b331119367dcc0d
张志军 <zjzhang2.iflytek.com>
2021-03-26 21:24:23 +0800

    car-list

--- pages/car/car-list/car-list.js
+++ pages/car/car-list/car-list.js
@@ -3,7 +3,7 @@ const { request } = require('../../../utils/ajax')
 Page({
   data: {
     value: '',
-    pageNum: 0,
+    pageNum: 1,
     pageSize: 6,
     size: 0,
     result: [],
@@ -12,18 +12,6 @@ Page({
   onLoad() {
     this.onSearch()
   },
-  onConfirm() {
-    this.selectComponent('#item').toggle();
-  },
-
-  onSwitch1Change({ detail }) {
-    this.setData({ switch1: detail });
-  },
-
-  onSwitch2Change({ detail }) {
-    this.setData({ switch2: detail });
-  },
-
   onChange(e) {
     this.setData({
       value: e.detail,
@@ -31,6 +19,8 @@ Page({
   },
   onSearch() {
     this.setData({ isLoading: true });
+    if (this.data.pageNum == 1)
+      this.data.result = []
     request({
       url: '/applets/vehicle_info/findVehicleInfosByPage',
       data: { vin: this.data.value, currenPage: this.data.pageNum, pageSize: this.data.pageSize },
@@ -46,26 +36,29 @@ Page({
           result: this.data.result.concat(res.data.data),
           size: res.data.recordsFiltered
         });
-        console.log('************1111', res);
       })
   },
   onNavigateAdd() {
-    wx.navigateTo({ url: '/pages/car/car-add/car-add' });
+    this.navigate('', '/pages/car/car-add/car-add');
   },
-  onNavigateDetail() {
-    wx.navigateTo({ url: '/pages/car/car-detail/car-detail' });
+  onNavigateDetail(e) {
+    this.navigate(e.currentTarget.dataset.vin, '/pages/car/car-detail/car-detail');
   },
-  onNavigateRepair() {
-    wx.navigateTo({ url: '/pages/car/car-repair/car-repair' });
+  onNavigateRepair(e) {
+    this.navigate(e.currentTarget.dataset.vin, '/pages/car/car-repair/car-repair');
   },
-  onNavigateDevice() {
-    wx.navigateTo({ url: '/pages/car/car-device/car-device' });
+  onNavigateDevice(e) {
+    this.navigate(e.currentTarget.dataset.vin, '/pages/car/car-device/car-device');
   },
-  onNavigateMap() {
-    wx.navigateTo({ url: '/pages/map/map-track/map-track' });
+  onNavigateMap(e) {
+    this.navigate(e.currentTarget.dataset.vin, '/pages/map/map-track/map-track');
   },
-  onNavigateSignal() {
-    wx.navigateTo({ url: '/pages/car/car-signal/car-signal' });
+  onNavigateSignal(e) {
+    this.navigate(e.currentTarget.dataset.vin, '/pages/car/car-signal/car-signal');
+  },
+  navigate(vin, url) {
+    wx.setStorage({ key: "vin", data: vin })
+    wx.navigateTo({ url: url });
   },
   noop() { },
   onReachBottom() {
