c9776fb5e060f1cf685575d540adfd864e37de54
张志军 <zjzhang2.iflytek.com>
2021-03-28 09:38:50 +0800

    request 交互优化

--- utils/ajax.js
+++ utils/ajax.js
@@ -1,7 +1,24 @@
+//接口地址host
 const baseUrl = "http://60.168.131.134:9001"
 
+var isLoading = false //防止重复提交
+
+function loading(isShow) {
+  isLoading = isShow
+  if (isShow)
+    wx.showLoading({ title: '加载中' })
+  else
+    wx.hideLoading()
+}
+
 // 封装request请求
-const request = (options, cb, that) => {
+const request = (options, cb) => {
+  if (isLoading)
+    return
+
+  let isShowLoading = options.method.toLowerCase() == 'post'
+  loading(isShowLoading)
+
   //url
   if (options.url.indexOf('http') != 0) {
     options.url = baseUrl + options.url;
@@ -15,6 +32,7 @@ const request = (options, cb, that) => {
         'tokenId': wx.getStorageSync('tokenId')
       },
       success(res) {
+        loading(false)
         if (res.statusCode === 401) {
           wx.redirectTo({ url: '/pages/login/login' });
           return
@@ -23,6 +41,7 @@ const request = (options, cb, that) => {
           cb(res.data)
       },
       fail(res) {
+        loading(false)
         wx.showToast({ icon: 'error', title: '请求失败' })
         if (cb)
           cb(res)
